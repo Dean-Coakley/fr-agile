e344fd32c7575997c2d4b8ba931828a307d59d45
Dean Coakley <dean.s.coakley@gmail.com>
2017-11-22 17:48:31 +0000

    Auth fix

--- push.sh
+++ push.sh
@@ -6,5 +6,5 @@ git checkout master
 git add .
 git commit -m "Travis build number: $TRAVIS_BUILD_NUMBER"
 
-git remote add dest-repo https://9bbc1599fc7d148b3867a23903a4d7dc0a35bf76@github.com/Raddish18/DeployRepo.git > /dev/null 2>&1
+git remote add dest-repo https://Raddish18:9bbc1599fc7d148b3867a23903a4d7dc0a35bf76@github.com/Raddish18/DeployRepo.git > /dev/null 2>&1
 git push --quiet --set-upstream dest-repo
