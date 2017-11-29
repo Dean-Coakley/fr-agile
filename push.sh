ef83ba646bb5b9b1d648894eec8cfd58569e80e8
Dean <dean.s.coakley@gmail.com>
2017-11-29 12:56:35 +0000

    Start build

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+git config --global user.name "Dean-Coakley"
+git config --global user.password "187628daed1700f96b5d75e48496ef9abd3c3772"
+
 git checkout master
 git add .
 git commit -m "Travis build number: $TRAVIS_BUILD_NUMBER"
 
-git remote add dest-repo https://187628daed1700f96b5d75e48496ef9abd3c3772@github.com/Dean-Coakley/DeployRepo.git > /dev/null 2>&1
+git remote add dest-repo https://Dean-Coakley:187628daed1700f96b5d75e48496ef9abd3c3772@github.com/Dean-Coakley/DeployRepo.git > /dev/null 2>&1
 git push --quiet --set-upstream dest-repo
