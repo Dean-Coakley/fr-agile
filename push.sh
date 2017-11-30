d0566aefcea6534df5fbda06a4dffdabf13bfe93
Dean <dean.s.coakley@gmail.com>
2017-11-30 10:50:25 +0000

    Use OAuth over personal access token

--- push.sh
+++ push.sh
@@ -6,5 +6,5 @@ git checkout master
 git add .
 git commit -m "Travis build number: $TRAVIS_BUILD_NUMBER"
 
-git remote add dest-repo https://Dean-Coakley:187628daed1700f96b5d75e48496ef9abd3c3772@github.com/Dean-Coakley/DeployRepo.git > /dev/null 2>&1
+git remote add dest-repo https://187628daed1700f96b5d75e48496ef9abd3c3772:x-oauth-basic@github.com/Dean-Coakley/DeployRepo.git > /dev/null 2>&1
 git push --quiet --set-upstream dest-repo
